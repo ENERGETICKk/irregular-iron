97a348c9baafe02d5f213a0094d9ef4d208c7c29
Adam El Ouahabi <105393528+ENERGETICKk@users.noreply.github.com>
2025-12-02 16:43:44 +0100

    Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless'; // O import vercel from '@astrojs/vercel';
 
-import vercel from '@astrojs/vercel';
-
-// La configuración de Node es necesaria para manejar las peticiones POST y GET dinámicas (tus API endpoints).
 export default defineConfig({
-  // Establece el modo de salida como 'server'.
-  // Esto hace que Astro compile tu proyecto para ser ejecutado en un servidor (Node.js en este caso).
-  // Define el adaptador que usará Astro para el modo 'server'.
-  // El adaptador @astrojs/node genera la lógica necesaria para correr el servidor de Node.js.
-  output: 'server',
-
-  adapter: vercel()
+  // 'server' es para SSR (formularios, APIs, etc)
+  output: 'server', 
+  
+  // Usamos el adaptador de Vercel
+  adapter: vercel(),
 });
